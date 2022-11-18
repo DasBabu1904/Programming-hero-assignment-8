ea4ef00e6075a758db544a2c2d8d17c38044799b
Dass7550 <dass7550@gmail.com>
2022-11-18 22:38:31 +0600

    cart is stil unsolved but working on it

--- src/components/CartArea/CartArea.js
+++ src/components/CartArea/CartArea.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import "../CartArea/CartArea.css"
 
 const CartArea = (props) => {
     const { CartList, ShowCart } = props;
-    ShowCart(CartList)
-    console.log(props)
+    // ShowCart(CartList)
+    // console.log(props)
     return (
-        <div>
-            <h1>This is cart</h1>
-
+        <div className="Cart_Section">
+            <h1> This is cart  {CartList.length}</h1>
+            {/* <div className="Cart_Section">cart section {CartList.length} </div> */}
         </div>
     );
 };
--- src/components/ShopArea/ShopArea.js
+++ src/components/ShopArea/ShopArea.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../ShopArea/ShopArea.css"
 import FetchData from '../FetchData/FetchData';
 import CartArea from '../CartArea/CartArea';
 const ShopArea = () => {
-    const CartList = [];
+    const [CartList, setCart] = useState([])
     function addToCart(item) {
-        CartList.push(item);
-        // console.log("New push", CartList)
+        const newCart = [...CartList, item];
+        setCart(newCart)
+        console.log("New push", CartList)
     }
+
     function ShowCart(CartList) {
-        console.log(CartList)
+        //console.log(CartList)
     }
     return (
 
         <div className="Shop_Section">
 
             <div className="Main_Section">Main Area <FetchData addToCart={addToCart} className="Grid_of_Card"></FetchData></div>
-            <div className="Cart_Section">cart section {CartList.map(item => {
-                <CartArea item={item} ShowCart={ShowCart}></CartArea>
-            })
-            }
-            </div>
+            <CartArea CartList={CartList}></CartArea>
         </div>
     );
 };
