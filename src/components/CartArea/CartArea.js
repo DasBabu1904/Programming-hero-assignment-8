16639eeef6685d466030fe37df65cf305a6ada41
Dass7550 <dass7550@gmail.com>
2022-11-24 23:05:01 +0600

    random selection of cart added and double call is solved. it was <React.StrictMode> i removed form index.js

--- src/components/CartArea/CartArea.js
+++ src/components/CartArea/CartArea.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import "../CartArea/CartArea.css"
 import CartElements from '../CartElements/CartElements';
+import RandomChoose from '../RandomChoose/RandomChoose';
 
 const CartArea = (props) => {
-    const { CartList, ShowCart } = props;
-    console.log(CartList);
+    const { CartList, ShowCart, ChooseOne, randIN } = props;
+
+    function randomElement() {
+        const x = parseInt((Math.random()) * 10) % CartList.length;
+        return x;
+    }
+    if (randIN >= 0) {
+        const Chose = CartList[randIN];
+        const newE = document.createElement('div');
+        const parent = document.getElementById("Cart_area")
+        newE.innerHTML = `
+            <p >
+            <h3 className="ElementStyle">${Chose.name}</h3>
+             <span className="Price_text">price: ${Chose.price}</span>
+             </p>
+             `
+        parent.innerHTML = ``
+        parent.appendChild(newE);
+    }
     return (
-        <div className="Cart_Section">
+        <div className="Cart_Section" >
             <div className="Sticky">
                 <h1> This is cart</h1>
                 {CartList.map(item => <CartElements item={item}></CartElements>)}
+                <button onClick={() => ChooseOne(randomElement())} className="Button_style">Chose For me</button>
+                <div id="Cart_area">
+
+                </div>
             </div>
         </div>
     );
--- src/components/ShopArea/ShopArea.js
+++ src/components/ShopArea/ShopArea.js
@@ -2,23 +2,27 @@ import React, { useState } from 'react';
 import "../ShopArea/ShopArea.css"
 import FetchData from '../FetchData/FetchData';
 import CartArea from '../CartArea/CartArea';
+import RandomChoose from '../RandomChoose/RandomChoose';
 const ShopArea = () => {
     const [CartList, setCart] = useState([])
     function addToCart(item) {
         const newCart = [...CartList, item];
         setCart(newCart)
-        console.log("New push", CartList)
+        //console.log("New push", CartList)
     }
 
-    function ShowCart(CartList) {
-        //console.log(CartList)
+    const [randIN, setIN] = useState(-1)
+    function ChooseOne(index) {
+        setIN(index)
+
     }
+
     return (
 
         <div className="Shop_Section">
 
             <div className="Main_Section">Main Area <FetchData addToCart={addToCart} className="Grid_of_Card"></FetchData></div>
-            <CartArea CartList={CartList}></CartArea>
+            <CartArea CartList={CartList} ChooseOne={ChooseOne} randIN={randIN}></CartArea>
         </div>
     );
 };
